1020b87e7a0e95300dc7987ed301342108f37333
Peter Soboyejo <thepcmrtim@gmail.com>
2016-12-10 15:26:20 -0500

    config example

---
+++ config.example.js
@@ -0,0 +1,25 @@
+{
+	"keywords": 'Soto',
+	"page": 'http://store.hufworldwide.com/collections/huf-x-ftp',
+	"size": "13",
+	"color": "Black",
+	"autoRetryOnCrash": true,
+	"cardInfo": {
+		"number": "4916 8068 7213 6192",
+		"nameOnCard": "Johnny Appleseed",
+		"mmyy": "05/2017",
+		"cvv": "928"
+	},
+	"billingInfo": {
+		"firstName": "Johnny",
+		"lastName": "Appleseed",
+		"company": null,
+		"address": "4384 Clement Street",
+		"aptNumber": null,
+		"city": "Atlanta",
+		"country": "United States",
+		"state": "GA",
+		"zipCode": "30303",
+		"phoneNumber": "+1 (404)-489-2804"
+	}
+}
